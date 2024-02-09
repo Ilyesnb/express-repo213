ba2dae6ea8e16cccfaf41841fae768698136d658
ilyesnb <ilyesnabi9@gmail.com>
2024-02-10 00:57:17 +0100

    last commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+app.get("/home",(req,res)=>{
+    res.redirect("/")
+})
+app.get("/buy",(req,res)=>{
+    res.render("buy")
+})
+app.get("/shop",(req,res)=>{
+    res.render("home")
+})
+app.all("*",(req,res)=>{
+    res.status(404).send("404 can't find the page!")
+})
+app.listen(3007,()=>{
+    console.log( "Server is running on port 3007");
+})
