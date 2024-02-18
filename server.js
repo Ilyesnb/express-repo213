e3908b6eeb5f77a8f714075f8d351ac4bc327747
ilyesnb <ilyesnabi9@gmail.com>
2024-02-19 00:03:05 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 app.set("view engine","ejs")
+app.use(express.static('public'))
 app.get("/",(req,res)=>{
     res.render("home")
 })
@@ -12,6 +13,9 @@ app.get("/buy",(req,res)=>{
 })
 app.get("/shop",(req,res)=>{
     res.render("shop")
+})
+app.get("/log-in",(req,res)=>{
+    res.render("log-in")
 })
 app.all("*",(req,res)=>{
     res.status(404).send("404 can't find the page!")
