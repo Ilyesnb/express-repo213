b188c4fbe74f1da4b5c3984db2ae148ac535e58d
ilyesnb <ilyesnabi9@gmail.com>
2024-02-25 18:59:22 +0100

    last commit

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 const {MongoClient }=require("mongodb");
-uri;
+// const uri ="mongodb+srv://ilyesnabi9:y0jr9NPwNLSr109z@cluster0.nyufwoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let dbConnection;
 
 module.exports= {
   connectToDb: (cb)=> {
   MongoClient.connect(uri)
  .then((client)=> {
-  dbConnection=client.db("Learning-DB");
+  dbConnection=client.db("code213");
   return cb();
  })
  .catch((err)=> {
--- server.js
+++ server.js
@@ -1,74 +1,64 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db/db");
-const database= " "
+const bodyParser = require('body-parser');
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 // Set view engine and static folder
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// Define routes
-const mybooks =[
-    {id:1 ,title:"The Alchemist", author:"Paulo Coelho"},  
-    {id:2 ,title:"Harry Potter and the Sorcerer's Stone", author: "J.K. Rowling"},
-    {id:3 ,title: 'The Catcher in the Rye', author:'J.D. Salinger'} 
-]
+let db
+connectToDb((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }else if (!err) {
+      app.listen(3007, () => {
+        console.log(`Server is running on port 3007`);
+    })
+    db=getDb()
+    }
+});
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 app.get("/home", (req, res) => {
   res.redirect("/");
 });
-
 app.get("/newProducts", (req, res) => {
   res.render("newProducts");
 });
-app.get("/products", (req, res) => {
-    res.render("products",{mybooks});
-  });
-app.get("/products",()=>{
-    const books = { id  , title , author};
-    mybooks.push(books)
-
+app.post("/newProducts",(req,res)=>{
+   const {text,body}=req.body;
+   const newBooks = { text , body };
+   db
+   .collection("books")
+   .insertOne(newBooks)
+   .then(()=>{
+    res.redirect("products");
+   })
+   .catch(()=> {
+    res.status(500).json({ error : "could not create new document"})
+   })
 
 })
-app.get("/prodects", (req, res) => {
-  let bookList = [];
-//   const database = getDb(); // Retrieve the database instance
-
-  database
+app.get("/products", (req, res) => {
+    let bookList = [];
+    db
     .collection("books")
     .find()
     .sort({ genres: 1 })
     .forEach((book) => bookList.push(book))
     .then(() => {
-      res.status(200).json(bookList);
+      // res.status(200).json(bookList);
+      res.render("products", { bookList });
     })
     .catch(() => {
-      res.status(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     });
-    res.json({msg:"Requesting books from API" });
 });
-
 app.get("/log-in", (req, res) => {
   res.render("log-in");
 });
-
 app.all("*", (req, res) => {
-  res.status(404).send("404 can't find the page!");
-});
-
-// Connect to the database and start the server
-connectToDb((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-    return;
-  }
-  database=getDb();
-
-});
-app.listen(3007, () => {
-  console.log(`Server is running on port 3007`);
-
+    res.status(404).send("404 can't find the page!");
 });
